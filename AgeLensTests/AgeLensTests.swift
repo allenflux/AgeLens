bb14819c09d9a535be8eca4c4f45ebdcf5d4f8ae
Flux <allenflux@gmail.com>
2026-06-17 21:58:40 +0700

    Initial Commit

---
+++ AgeLensTests/AgeLensTests.swift
@@ -0,0 +1,19 @@
+//
+//  AgeLensTests.swift
+//  AgeLensTests
+//
+//  Created by AllenFlux on 2026/6/17.
+//
+
+import Testing
+@testable import AgeLens
+
+struct AgeLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
